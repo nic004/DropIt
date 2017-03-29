8a28c731ed21f6ff4d0a247b5b74c35c4391341c
nathan <nathan@oz1.com>
2017-03-29 16:18:47 +0900

    .

--- DropIt/DITAddMonthlyManagementItemViewController.swift
+++ DropIt/DITAddMonthlyManagementItemViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import PickerController
+import CoreStore
 
 class DITAddMonthlyManagementItemViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var monthField: UITextField!
     @IBOutlet var startDayField: UITextField!
     @IBOutlet var endDayField: UITextField!
+    let dateFormat = "yyyy. MM. dd."
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +36,44 @@ class DITAddMonthlyManagementItemViewController: UIViewController, UITextFieldDe
     }
     */
     
+    func dateFrom(text: String?) -> Date? {
+        guard let dateString = text else {
+            return nil
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        let date = dateFormatter.date(from: dateString)!
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: date)
+        return calendar.date(from: components)
+    }
+    
+    @IBAction func saveAction(_ sender: Any) {
+        guard let begin = dateFrom(text: startDayField.text), let end = dateFrom(text: endDayField.text) else {
+            return
+        }
+        
+        CoreStore.beginAsynchronous {
+            let amount = $0.create(Into<Aggregation>())
+            amount.title = self.monthField.text
+            amount.begin = begin as NSDate?
+            amount.end = end as NSDate?
+            $0.commit()
+        }
+        performSegue(withIdentifier: "unwindToMonthlyManagement", sender: self)
+    }
+    
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField === startDayField || textField === endDayField {
+            pickDateFor(textField: textField)
+        } else if textField === monthField {
+            pickMonthFor(textField: textField)
+        }
+        return false
+    }
+    
+    func pickDateFor(textField: UITextField) {
         let pickerController = PickerController_Date(title: "", onDone: { [unowned self] (date) in
             textField.text = date.toString(format: "yyyy. MM. dd.")
             textField.resignFirstResponder()
@@ -57,6 +97,15 @@ class DITAddMonthlyManagementItemViewController: UIViewController, UITextFieldDe
             pickerController.setDate(date: Date())
         }
         
-        return false
+    }
+    
+    func pickMonthFor(textField: UITextField) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM"
+        formatter.locale = Locale.current
+        let currentMonthIndex = Calendar.current.component(.month, from: Date()) - 1
+        showGroupPicker(title: "Month", groupData: [(0..<12).map { formatter.monthSymbols[$0]}], selectedIndices: [currentMonthIndex], onDone: { (indicies, items) in
+            textField.text = items[0]
+        }, onCancel: nil)
     }
 }
